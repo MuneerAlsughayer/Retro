757f39655943b3be95eecd6f8bc91ec376902fd2
Muneer <Muneeralsughayer@outlook.com>
2025-04-23 00:01:42 +0300

    Update Variable_1 = 1.py

--- Variable_1 = 1.py
+++ Variable_1 = 1.py
@@ -9,10 +9,11 @@ while i <10:
     print ('iteration number', i)
     i+=1
 
+for string in ['ONE', 'TWO']:
+    if string is 'TWO':
+        print ('O Found')
+
 for i in [1, 2, 3, 4, 5, 6]:
     print ('6 passes done')
 
 
-for i in ['ONE TWO']:
-    if i is 'O':
-        print ('O Found')
